6aad68216d2506bb0a8088d609da8fdcbdc9e49e
xjavz <7vjavster@gmail.com>
2022-09-18 18:58:46 -0600

    Scripts

--- Balloon Pop Game/Assets/Scripts/FloatUp.cs
+++ Balloon Pop Game/Assets/Scripts/FloatUp.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Float : MonoBehaviour
+public class FloatUp : MonoBehaviour
 {
+    
+    public float moveSpeed; // Speed at which balloon will move upwards
+    
+    public float upperBound; // Top boundary for when balloon exits the screen it will get destroyed
+    private Balloon balloon; // Reference the balloon gameObject
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       balloon = GetComponent<Balloon>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Move the balloon upwards
+        transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+
+        //Destroy balloon if it passes the top boundry
+        if(transform.position.y > upperBound)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Balloon Pop Game/Assets/Scripts/SpawnManager.cs
+++ Balloon Pop Game/Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    // Global Variables
+    public gameObject[] balloonPrefabs;
+    public float startDelay = 0.5f;
+    public float spawnInterval = 1.5f;
+
+    public float xRange = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomBalloon", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+
+    void SpawnRandomBalloon()
+    {
+        // Get a random position on the x-axis
+        Vector3 spawnPosX = new Vector3(Random.Range(-xRange,xRange),0,0); 
+
+        // Pick a random balloon from the ballon array
+        int balloonIndex = Random.Range(0,balloonPrefabs.Length);
+
+        // Spawn a random ballon at spawn point
+        Instantiate(balloonPrefabs[balloonIndex], spawnPosX, ballonPrefabs[balloonIndex].transform.rotation);
         
     }
 }
