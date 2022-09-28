cf24866ac83868d950c740aad3835eb83ef67207
xjavz <7vjavster@gmail.com>
2022-09-28 03:34:36 -0600

    Setup EnemySpawner, UFOs, and Physics

---
+++ UFO Defense Force/Assets/Scripts/EnemySpawnManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnManager : MonoBehaviour
+{
+    
+    public GameObject[] enemyPrefabs;
+    private float spawnRangeX;
+    private float spawnPosZ;
+    
+    private float startDelay = 2f;
+    private float spawnInterval = 2f;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomEnemy", startDelay, spawnInterval);
+    }
+
+   
+     void SpawnRandomEnemy()
+    {
+        // Generate a position to spawn at 
+        Vector3 spawnPos = new Vector3 (Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        // Pick a random/ ufo from the array
+        int enemyIndex = Random.Range(0,enemyPrefabs.Length);
+        // Spawn the enemy indexed from the array
+        Instantiate(enemyPrefabs[enemyIndex], spawnPos, enemyPrefabs[enemyIndex].transform.rotation);
+   }
+}
